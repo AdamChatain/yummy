c5c2c3bbc29812eae56596920239daed24ef9867
AdamChatain <77132979+AdamChatain@users.noreply.github.com>
2021-01-07 20:40:25 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,73 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        # . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # # # # #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        . . . . #
+        . . . . #
+        `)
+})
